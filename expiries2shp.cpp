3ec6ae2e07fff0086234ea21802ad4dca3fe520d
Michael Reichert <michael.reichert@geofabrik.de>
2016-10-07 14:43:33 +0200

    add better help

--- expiries2shp.cpp
+++ expiries2shp.cpp
@@ -35,20 +35,6 @@ OGRLayer* set_up_layer(std::string& out_directory, std::string& layer_name, OGRD
         std::cerr << "Creating zoom field failed.\n";
         exit(1);
     }
-    OGRFieldDefn x_field("x", OFTInteger);
-    x_field.SetWidth(7);
-    ogrerr = layer->CreateField(&x_field);
-    if (ogrerr != OGRERR_NONE) {
-        std::cerr << "Creating x field failed.\n";
-        exit(1);
-    }
-    OGRFieldDefn y_field("y", OFTInteger);
-    y_field.SetWidth(3);
-    ogrerr = layer->CreateField(&y_field);
-    if (ogrerr != OGRERR_NONE) {
-        std::cerr << "Creating y field failed.\n";
-        exit(1);
-    }
     layer->StartTransaction();
 
     std::ofstream file;
@@ -62,6 +48,14 @@ OGRLayer* set_up_layer(std::string& out_directory, std::string& layer_name, OGRD
     file << "UTF-8" << std::endl;
     file.close();
     return layer;
+}
+
+void print_help() {
+    std::cerr << "USAGE:\nexpiries2shp [OPTIONS] [INFILE OUT_DIRECTORY]\n" \
+              << "\nOptions:\n" \
+              << "  -p, --projection     Use other projection than EPSG:3857 for output.\n" \
+              << "  -s, --sequence       Sequence number of the diff file which was used to\n" \
+              << "                       create this expiry list.\n";
 }
 
 int main(int argc, char* argv[]) {
@@ -82,12 +76,12 @@ int main(int argc, char* argv[]) {
         switch (c) {
         case 'p':
             epsg = atoi(optarg);
-            std::cout << "Using output SRS EPSG:" << epsg << std::endl;
             break;
         case 's':
             sequence = optarg;
             break;
         default:
+            print_help();
             exit(1);
         }
     }
@@ -95,8 +89,8 @@ int main(int argc, char* argv[]) {
     std::string input_filename = "";
     std::string output_directory = "";
     if (remaining_args != 2) {
-            std::cerr << "Usage: " << argv[0] << " [OPTIONS] [INFILE OUT_DIRECTORY]" << std::endl;
-            exit(1);
+        print_help();
+        exit(1);
     } else {
         input_filename =  argv[optind];
         output_directory = argv[optind+1];
@@ -138,9 +132,7 @@ int main(int argc, char* argv[]) {
         boost::split(elements, line, boost::is_any_of("/"));
 
         int zoom = std::stoi(elements.at(0));
-        int x = std::stoi(elements.at(1));
-        int y = std::stoi(elements.at(2));
-        Tile tile(x, y, zoom);
+        Tile tile(std::stoi(elements.at(1)), std::stoi(elements.at(2)), zoom);
         std::unique_ptr<OGRPolygon> polygon = tile.get_square();
         OGRFeature* feature = OGRFeature::CreateFeature(layer->GetLayerDefn());
         if (epsg != 3857) {
@@ -150,8 +142,6 @@ int main(int argc, char* argv[]) {
         feature->SetGeometry(polygon.get());
         feature->SetField("sequence", sequence.c_str());
         feature->SetField("zoom", zoom);
-        feature->SetField("x", x);
-        feature->SetField("y", y);
         if (layer->CreateFeature(feature) != OGRERR_NONE) {
             std::cerr << "Failed to create feature.\n";
             exit(1);
