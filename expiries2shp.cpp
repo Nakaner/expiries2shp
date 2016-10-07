ac512c1b4514e14d88e38801557eb292ace7054e
Michael Reichert <michael.reichert@geofabrik.de>
2016-10-07 16:04:32 +0200

    add option for tile IDs to be added to the shapefile

--- expiries2shp.cpp
+++ expiries2shp.cpp
@@ -15,7 +15,8 @@
 #include <boost/algorithm/string.hpp>
 #include "tile.hpp"
 
-OGRLayer* set_up_layer(std::string& out_directory, std::string& layer_name, OGRDataSource* data_source, OGRSpatialReference* output_srs) {
+OGRLayer* set_up_layer(std::string& out_directory, std::string& layer_name, OGRDataSource* data_source,
+        OGRSpatialReference* output_srs, bool tile_ids) {
     OGRLayer *layer = data_source->CreateLayer(layer_name.c_str(), output_srs, wkbPolygon, NULL);
     if (!layer) {
         std::cerr << "Creating layer " << layer_name << " failed.\n";
@@ -34,6 +35,22 @@ OGRLayer* set_up_layer(std::string& out_directory, std::string& layer_name, OGRD
     if (ogrerr != OGRERR_NONE) {
         std::cerr << "Creating zoom field failed.\n";
         exit(1);
+    }
+    if (tile_ids) {
+        OGRFieldDefn x_field("x", OFTInteger);
+        x_field.SetWidth(8);
+        ogrerr = layer->CreateField(&x_field);
+        if (ogrerr != OGRERR_NONE) {
+            std::cerr << "Creating x field failed.\n";
+            exit(1);
+        }
+        OGRFieldDefn y_field("y", OFTInteger);
+        y_field.SetWidth(8);
+        ogrerr = layer->CreateField(&y_field);
+        if (ogrerr != OGRERR_NONE) {
+            std::cerr << "Creating y field failed.\n";
+            exit(1);
+        }
     }
     layer->StartTransaction();
 
@@ -55,6 +72,7 @@ void print_help() {
               << "\nOptions:\n" \
               << "  -p, --projection     Use other projection than EPSG:3857 for output.\n" \
               << "  -s, --sequence       Sequence number of the diff file which was used to\n" \
+              << "  -i, --ids            Add columns with x and y index of the tile\n" \
               << "                       create this expiry list.\n";
 }
 
@@ -63,12 +81,14 @@ int main(int argc, char* argv[]) {
     static struct option long_options[] = {
         {"projection", required_argument, 0, 'p'},
         {"sequence", required_argument, 0, 's'},
+        {"ids", no_argument, 0, 'i'},
         {0, 0, 0, 0}
     };
     int epsg = 3857;
     std::string sequence = "0";
+    bool tile_ids = false;
     while (true) {
-        int c = getopt_long(argc, argv, "p:s:", long_options, 0);
+        int c = getopt_long(argc, argv, "p:s:i", long_options, 0);
         if (c == -1) {
             break;
         }
@@ -79,6 +99,9 @@ int main(int argc, char* argv[]) {
             break;
         case 's':
             sequence = optarg;
+            break;
+        case 'i':
+            tile_ids = true;
             break;
         default:
             print_help();
@@ -121,7 +144,7 @@ int main(int argc, char* argv[]) {
     web_mercator_srs.importFromEPSG(3857);
     OGRSpatialReference output_srs;
     output_srs.importFromEPSG(epsg);
-    OGRLayer* layer = set_up_layer(output_directory, sequence, data_source, &output_srs);
+    OGRLayer* layer = set_up_layer(output_directory, sequence, data_source, &output_srs, tile_ids);
 
 
     // read expiry file and write shape file
@@ -132,7 +155,9 @@ int main(int argc, char* argv[]) {
         boost::split(elements, line, boost::is_any_of("/"));
 
         int zoom = std::stoi(elements.at(0));
-        Tile tile(std::stoi(elements.at(1)), std::stoi(elements.at(2)), zoom);
+        int x = std::stoi(elements.at(1));
+        int y = std::stoi(elements.at(2));
+        Tile tile(x, y, zoom);
         std::unique_ptr<OGRPolygon> polygon = tile.get_square();
         OGRFeature* feature = OGRFeature::CreateFeature(layer->GetLayerDefn());
         if (epsg != 3857) {
@@ -142,6 +167,10 @@ int main(int argc, char* argv[]) {
         feature->SetGeometry(polygon.get());
         feature->SetField("sequence", sequence.c_str());
         feature->SetField("zoom", zoom);
+        if (tile_ids) {
+            feature->SetField("x", x);
+            feature->SetField("y", y);
+        }
         if (layer->CreateFeature(feature) != OGRERR_NONE) {
             std::cerr << "Failed to create feature.\n";
             exit(1);
